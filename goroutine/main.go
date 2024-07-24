3b5450dfadb85f48558ffe49154d0e52733fdfc2
Benediktus Satriya <bensatriya3@gmail.com>
2024-07-24 10:36:11 +0700

    feat: waiting process goroutine with waitgroup

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"sync/atomic"
-	"time"
 )
 
 var balance int
 var balanceSafe atomic.Int32
 
 func main() {
-
+	wg := &sync.WaitGroup{}
 	balance = 10000
 	balanceSafe.Add(10000)
 
@@ -18,24 +18,27 @@ func main() {
 
 	// blast transfer with goroutine
 	for i := 1; i <= 1000; i++ {
-		go transfer(10)
-		go transferSafe(10)
+		wg.Add(2)
+		go transfer(10, wg)
+		go transferSafe(10, wg)
 	}
 
 	// the current balance after transfer
 	// should be 0, because 1.000 * 10 = 10.000
 	// then 10.000 - 10.000 = 0
-	time.Sleep(1 * time.Second)
+	wg.Wait()
 	fmt.Println("current balance not safe: ", balance)
 	fmt.Println("current balance safe: ", balanceSafe.Load())
 }
 
 // this func is not thread safe
-func transfer(value int) {
+func transfer(value int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	balance -= value
 }
 
 // this func thread safe
-func transferSafe(value int32) {
+func transferSafe(value int32, wg *sync.WaitGroup) {
+	defer wg.Done()
 	balanceSafe.Add(-value)
 }
