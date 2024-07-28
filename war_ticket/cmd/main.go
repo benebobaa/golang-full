00f8a36c71f8113efdb6d44d13d104f266e64f8a
Benediktus Satriya <bensatriya3@gmail.com>
2024-07-28 17:14:51 +0700

    feat: gracefull shutdown, 5 seconds timeout

--- war_ticket/cmd/main.go
+++ war_ticket/cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -16,6 +21,33 @@ func main() {
 		Handler: router,
 	}
 
-	log.Println("Starting server on port 8080")
-	server.ListenAndServe()
+	go func() {
+		log.Println("Starting server...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Waiting signal send to chan quit
+	// Blocking channel
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+
+	log.Println("Server exiting")
 }
